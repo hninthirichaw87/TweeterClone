44aa3fe0f09051b84bfa310f3fef8a06d8c4e28a
Hnin Thiri Chaw <12230533+hninthirichaw87@users.noreply.github.com>
2023-10-24 22:18:29 +0700

    Update appApp.py

--- appApp.py
+++ appApp.py
@@ -4,13 +4,13 @@ from datetime import datetime
 
 #configuration key
 firebaseConfig = {
-  "apiKey": "AIzaSyBRWmAyFNh71lagrb4TkbU-lmCXULwkXOQ",
-  "authDomain": "fir-stapp-8978b.firebaseapp.com",
-  "databaseURL": "https://fir-stapp-8978b-default-rtdb.europe-west1.firebasedatabase.app/",
-  "storageBucket": "fir-stapp-8978b.appspot.com",
-  "messagingSenderId": "449510012162",
-  "appId": "1:449510012162:web:c41d2c63a6e65b8211fe1b",
-  "measurementId": "G-T7TFCT7YR2"}
+  "apiKey": "",
+  "authDomain": "",
+  "databaseURL": "",
+  "storageBucket": "",
+  "messagingSenderId": "",
+  "appId": "",
+  "measurementId": ""}
 
 #firebase authentication
 
